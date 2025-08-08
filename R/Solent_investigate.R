af01424ce0bcff3f825df99fb3df849ad1cd960d
chriscesar <chris.cesar@environment-agency.gov.uk>
2025-08-08 15:28:46 +0100

    Updated formatting of data for more general use

--- R/Solent_investigate.R
+++ R/Solent_investigate.R
@@ -13,40 +13,54 @@ source("R/000setup.R")
 
 # load data ####
 tic("Load data")
-dfsol <- readRDS(file="outputs/Phyto_2000_2025_with_Lifeforms_USE_updateCalcs.Rdat") %>% 
-  janitor::clean_names() %>% 
-  dplyr::filter(str_detect(water_body, "^SOLENT")) %>% 
-  dplyr::select(-c(
-    region, area,sample_time,sample_date_time,
-    sample_reason_primary,analysis_id,
-    replicate_code,settling_time,taxon_qualifier,
-    colonies_per_litre_millilitre,sample_ngr,
-    sample_method,wfd_waterbody_id,catchment,
-    version,analysis_type,analysis_method,date_of_analysis,
-    distance_to_wb,eastings,northings,aphia_id,
-    aphia_id_2, scientificname,status, unacceptreason,
-    taxon_rank_id,rank,parent_name_usage_id_x,
-    is_marine,is_brackish,is_freshwater,is_terrestrial,is_extinct,
-    x2,parent_name_usage_id_y,taxa_reported,valid_name,size_class,
-    qa_flag))
-    
+# df0_raw <- readRDS(file="outputs/Phyto_2000_2025_with_Lifeforms_USE_updateCalcs.Rdat") %>% 
+#   janitor::clean_names() #
+# df0 <- df0_raw %>% 
+#   # dplyr::filter(str_detect(water_body, "^SOLENT")) %>% 
+#   dplyr::select(-c(
+#     region, area,sample_time,sample_date_time,
+#     sample_reason_primary,analysis_id,
+#     replicate_code,settling_time,taxon_qualifier,
+#     colonies_per_litre_millilitre,sample_ngr,
+#     sample_method,wfd_waterbody_id,catchment,
+#     version,analysis_type,analysis_method,date_of_analysis,
+#     distance_to_wb,eastings,northings,aphia_id,
+#     aphia_id_2, scientificname,status, unacceptreason,
+#     taxon_rank_id,rank,parent_name_usage_id_x,
+#     is_marine,is_brackish,is_freshwater,is_terrestrial,is_extinct,
+#     x2,parent_name_usage_id_y,taxa_reported,valid_name,size_class,
+#     qa_flag))
+df0_raw <- readRDS("outputs/Phyto_2000_2025_with_Lifeforms_USE_updateCalcs_updateWB.Rdat")
 toc(log=TRUE)
 
-dfsol %>% 
+df0_raw %>%
   dplyr::select(
-    river_basi:surveillan,
+    rbd,
+    wbid, 
+    wb_name,
+    wb_type,
+    hm_desig,
+    area_name,
+    man_catch,
+    op_catch,
+    typology,
+    site_id, biosys_code, biosys_short, site_station_name,
+    sample_date,
+    replicate_code, settling_time, sample_method, 
+    colonies_per_litre_millilitre, analysis_type, analysis_method,
+    date_of_analysis, surveillan,
     cells_per_litre,
     cells_per_m3,
-    #mean_vol_per_cell_um3,
-    #median_vol_per_cell_um3,
-    #mean_c_per_cell_pg_c,
-    #mean_c_per_cell_ug_c,
-    #mean_c_per_cell_mg_c,
-    #median_c_per_cell_pg_c,
-    #median_c_per_cell_ug_c,
-    #median_c_per_cell_mg_c,
-    #tot_mn_vol_um3,
-    #tot_md_vol_um3,
+    #mean_vol_per_cell_um3
+    #median_vol_per_cell_um3
+    #mean_c_per_cell_pg_c
+    #mean_c_per_cell_ug_c
+    #mean_c_per_cell_mg_c
+    #median_c_per_cell_pg_c
+    #median_c_per_cell_ug_c
+    #median_c_per_cell_mg_c
+    #tot_mn_vol_um3
+    #tot_md_vol_um3
     mn_carbon_tot_pg_c_per_litre,
     mn_carbon_tot_ug_c_per_litre,
     mn_carbon_tot_mg_c_per_litre,
@@ -58,8 +72,7 @@ dfsol %>%
     md_carbon_tot_mg_c_per_litre,
     md_carbon_tot_pg_c_per_m3,
     md_carbon_tot_ug_c_per_m3,
-    md_carbon_tot_mg_c_per_m3
-    ) %>% 
+    md_carbon_tot_mg_c_per_m3) %>% 
   dplyr::group_by(dplyr::across(-c(
     cells_per_litre,
     cells_per_m3,
@@ -95,27 +108,86 @@ dfsol %>%
 
 write.csv(
   dfsummary,
-  file = "outputs/Phyto_Solent.csv",
+  file = "outputs/Phyto_by_sample.csv",
   row.names = FALSE
   )
 toc(log=TRUE)
 
+### SOLENT
 dfsummary %>% 
   dplyr::filter(sample_date > "2008-01-01") %>% 
+  dplyr::filter(wb_name == "Solent") %>% 
   # dplyr::filter(!is.na(biosys_code)) %>% 
   ggplot(., aes(
     x = sample_date,
-    y = log10(mn_carbon_tot_mg_c_per_m3)
+    y = log10(md_carbon_tot_mg_c_per_m3)
     )) +
   geom_point()+
   geom_hline(yintercept = log10(7.5),lty=2)+
   #facet_wrap(. ~ wb_name) +
   geom_smooth(method = "gam")+
-  labs(title = "Log10 carbon content per sample over time in the Solent",
-       caption = "Blue line indicates GAM smoother\nDashed line represents log10(7.5)")+
+  labs(title = "Log10 carbon content per phytoplankton sample over time in the Solent",
+       caption = "Blue line indicates GAM smoother\nDashed line represents log10(7.5)",
+       y = bquote(Log[10]~Total~carbon~content~(as~mg~carbon~m^-3)))+
   theme(
     axis.title.x = element_blank(),
     axis.text.x =  element_text(face=2),
     axis.title.y = element_text(face=2)
   )
   
+### Thames
+dfsummary %>% 
+  dplyr::filter(sample_date > "2008-01-01") %>% 
+  dplyr::filter(stringr::str_detect(wb_name, stringr::regex("Thames", ignore_case = TRUE))) %>% 
+  # dplyr::filter(!is.na(biosys_code)) %>% 
+  ggplot(., aes(
+    x = sample_date,
+    y = log10(md_carbon_tot_mg_c_per_m3)
+  )) +
+  geom_point()+
+  geom_hline(yintercept = log10(7.5),lty=2)+
+  #facet_wrap(. ~ wb_name) +
+  geom_smooth(method = "gam")+
+  facet_wrap(.~wb_name)+
+  labs(title = "Log10 carbon content per phytoplankton sample over time in Thames water bodies",
+       caption = "Blue line indicates GAM smoother\nDashed line represents log10(7.5)",
+       y = bquote(Log[10]~Total~carbon~content~(as~mg~carbon~m^-3)))+
+  theme(
+    axis.title.x = element_blank(),
+    strip.text = element_text(face=2),
+    axis.text.x =  element_text(face=2),
+    axis.title.y = element_text(face=2)
+  )
+
+### Essex & Kent North
+dfsummary %>% 
+  # dplyr::filter(sample_date > "2008-01-01") %>% 
+  dplyr::filter(stringr::str_detect(
+    wb_name,
+    stringr::regex("^(Thames|Essex|Kent North)", ignore_case = TRUE)
+  )) %>% 
+  # dplyr::filter(!is.na(biosys_code)) %>% 
+  ggplot(., aes(
+    x = sample_date,
+    y = log10(md_carbon_tot_mg_c_per_m3)
+  )) +
+  geom_point(
+    #aes(colour = wb_name)
+  )+
+  geom_hline(yintercept = log10(7.5),lty=2)+
+  #facet_wrap(. ~ wb_name) +
+  geom_smooth(
+    # aes(colour = wb_name),
+    method = "gam",
+    se=FALSE
+    )+
+  #facet_wrap(.~wb_name)+
+  labs(title = "Log10 carbon content per phytoplankton sample over time in Thames water bodies",
+       caption = "Blue line indicates GAM smoother\nDashed line represents log10(7.5)",
+       y = bquote(Log[10]~Total~carbon~content~(as~mg~carbon~m^-3)))+
+  theme(
+    axis.title.x = element_blank(),
+    strip.text = element_text(face=2),
+    axis.text.x =  element_text(face=2),
+    axis.title.y = element_text(face=2)
+  )
