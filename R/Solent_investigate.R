57dbb16d97a31119005b36139d6cd71af9952759
chriscesar <chris.cesar@environment-agency.gov.uk>
2025-08-11 15:29:41 +0100

    Ordered RBD factor; Plotted total C for all WBs; Initial plots of Diatom:Dinoflag

--- R/Solent_investigate.R
+++ R/Solent_investigate.R
@@ -33,6 +33,7 @@ tic("Load data")
 df0_raw <- readRDS("outputs/Phyto_2000_2025_with_Lifeforms_USE_updateCalcs_updateWB.Rdat")
 toc(log=TRUE)
 
+tic("Sum carbon across taxa")
 df0_raw %>%
   dplyr::select(
     rbd,
@@ -104,7 +105,24 @@ df0_raw %>%
     md_carbon_tot_pg_c_per_m3 = sum(md_carbon_tot_pg_c_per_m3, na.rm=TRUE),
     md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3, na.rm=TRUE),
     md_carbon_tot_mg_c_per_m3 = sum(md_carbon_tot_mg_c_per_m3, na.rm=TRUE),
-    .groups = "drop") -> dfsummary
+    .groups = "drop") %>%# names()
+  dplyr::mutate(rbd = factor(rbd,
+                             levels = c(
+                               "Northumbria",
+                               "Humber",
+                               "Anglian",
+                               "Thames",
+                               "South East",
+                               "South West",
+                               "Severn",
+                               "North West",
+                               "Solway Tweed"
+                               )
+                             )) %>% 
+  dplyr::mutate(wb_name = factor(wb_name,
+                                 levels = unique(wb_name[order(rbd)])
+                                 )
+                ) -> dfsummary
 
 write.csv(
   dfsummary,
@@ -190,4 +208,176 @@ dfsummary %>%
     strip.text = element_text(face=2),
     axis.text.x =  element_text(face=2),
     axis.title.y = element_text(face=2)
+  )
+
+# Total C all WBs ####
+# throw away WBs with fewer than this number of samples:
+number <- 500
+
+dfsummary %>% 
+  dplyr::group_by(wb_name) %>%
+  dplyr::filter(n() >= number) %>%
+  dplyr::filter(sample_date > "2008-01-01") %>% 
+  ggplot(.,
+         aes(
+           x = sample_date,
+           y = log10(md_carbon_tot_mg_c_per_m3)
+         )) +
+  geom_point(
+    # aes(colour = wb_name),show.legend = FALSE,
+    alpha=0.25
+    )+
+  # facet_wrap(.~wb_name) +
+  facet_wrap(.~rbd) +
+  labs(title = bquote(bold(Log[10]~carbon~content~per~phytoplankton~sample~over~time~'in'~selected~EA~water~bodies)),
+       caption = "Blue line indicates GAM smoother\nDashed line represents log10(7.5)",
+       y = bquote(bold(Log[10]~Total~carbon~content~(as~mg~carbon~m^-3))))+
+  geom_hline(yintercept = log10(7.5), lty = 2)+
+  geom_smooth(
+    aes(colour = wb_name),show.legend = FALSE,
+    method = "gam",
+    se = FALSE
+  ) +
+  geom_smooth(
+    linewidth = 2,
+    method = "gam",
+    se = FALSE
+  ) +
+  theme(
+    axis.title.x = element_blank(),
+    strip.text = element_text(face=2)
+  )
+         
+
+## diatom - dinof ratio
+df0_raw %>% 
+  # remove non-diatom/non-dinoflagellate
+  dplyr::filter(phyto_lf == "Diatom"|phyto_lf == "Dinoflagellate") %>% 
+  dplyr::select(
+    rbd,
+    wbid, 
+    wb_name,
+    wb_type,
+    hm_desig,
+    area_name,
+    man_catch,
+    op_catch,
+    typology,
+    site_id, biosys_code, biosys_short, site_station_name,
+    sample_date,
+    phyto_lf,
+    replicate_code, settling_time, sample_method, 
+    colonies_per_litre_millilitre, analysis_type, analysis_method,
+    date_of_analysis, surveillan,
+    cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3) %>% 
+  dplyr::group_by(dplyr::across(-c(
+    cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3
+  ))) %>% 
+  dplyr::summarise(
+    cells_per_litre = sum(cells_per_litre, na.rm = FALSE),
+    cells_per_m3 = sum(cells_per_m3, na.rm = FALSE),
+    mn_carbon_tot_pg_c_per_litre = sum(mn_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_litre = sum(mn_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_litre = sum(mn_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_pg_c_per_m3 = sum(mn_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_m3 = sum(mn_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_m3 = sum(mn_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_litre = sum(md_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_litre = sum(md_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_litre = sum(md_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_m3 = sum(md_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_m3 = sum(md_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    .groups = "drop") %>%# names()
+  dplyr::mutate(rbd = factor(rbd,
+                             levels = c(
+                               "Northumbria",
+                               "Humber",
+                               "Anglian",
+                               "Thames",
+                               "South East",
+                               "South West",
+                               "Severn",
+                               "North West",
+                               "Solway Tweed"
+                             )
+  )) %>% #names()
+  dplyr::mutate(wb_name = factor(wb_name,
+                                 levels = unique(wb_name[order(rbd)])
+                                 )
+                ) %>% 
+  # widen for calculation of ratio
+  tidyr::pivot_wider(
+    names_from = phyto_lf,
+    values_from = c(cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3)
+    ) %>% 
+  dplyr::mutate(
+    ratio_md_carbon_tot_mg_c_per_m3 = (md_carbon_tot_mg_c_per_m3_Diatom/md_carbon_tot_mg_c_per_m3_Dinoflagellate)) %>% 
+  # remove NA and Inf values
+  dplyr::filter(!is.na(ratio_md_carbon_tot_mg_c_per_m3)) %>% 
+  dplyr::filter(!is.infinite(ratio_md_carbon_tot_mg_c_per_m3)) %>% 
+  group_by(wb_name) %>% dplyr::filter(n() >= number) %>% ungroup() %>% 
+  dplyr::filter(sample_date > "2008-01-01") %>% 
+  ggplot(aes(
+    x = sample_date,
+    y = log10(ratio_md_carbon_tot_mg_c_per_m3)
+  )
+  )+
+  geom_point(
+    alpha = 0.25
+  )+
+  facet_wrap(.~rbd)+
+  geom_smooth(
+    method="gam",
+    se=TRUE
+    )+
+  labs(title =
+         bquote(bold(Ratio~of~carbon~content~'in'~diatoms~taxa~to~that~'in'~donoflagellates)),
+       y= bquote(bold(Log[10]~ratio~of~diatom~carbon~to~dinoflagellate~carbon)
+                 ),
+       caption="Blue line indicates GAM smoother"
+       )+
+  theme(
+    axis.title.x = element_blank(),
+    axis.text = element_text(face=2),
+    strip.text = element_text(face=2)
   )
