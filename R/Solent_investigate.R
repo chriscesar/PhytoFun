5d0efdbbd1f333d170dba7df3323290e1fc628a7
chriscesar <chris.cesar@environment-agency.gov.uk>
2025-08-13 16:17:51 +0100

    Heatmap plots by lifeforms & taxa

--- R/Solent_investigate.R
+++ R/Solent_investigate.R
@@ -390,3 +390,292 @@ df0_raw %>%
     axis.text = element_text(face=2),
     strip.text = element_text(face=2)
   )
+###########
+
+# by lifeform ####
+tic("Sum carbon across taxa")
+df0_raw %>%
+  dplyr::select(
+    rbd,
+    wbid, 
+    wb_name,
+    wb_type,
+    hm_desig,
+    area_name,
+    man_catch,
+    op_catch,
+    typology,
+    site_id, biosys_code, biosys_short, site_station_name,
+    eastings,northings,
+    sample_date,
+    replicate_code, 
+    phyto_lf,
+    colonies_per_litre_millilitre, analysis_type, analysis_method,
+    date_of_analysis, surveillan,
+    cells_per_litre,
+    cells_per_m3,
+    #mean_vol_per_cell_um3
+    #median_vol_per_cell_um3
+    #mean_c_per_cell_pg_c
+    #mean_c_per_cell_ug_c
+    #mean_c_per_cell_mg_c
+    #median_c_per_cell_pg_c
+    #median_c_per_cell_ug_c
+    #median_c_per_cell_mg_c
+    #tot_mn_vol_um3
+    #tot_md_vol_um3
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3) %>% 
+  dplyr::group_by(dplyr::across(-c(
+    cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3
+  ))) %>% 
+  dplyr::summarise(
+    cells_per_litre = sum(cells_per_litre, na.rm = FALSE),
+    cells_per_m3 = sum(cells_per_m3, na.rm = FALSE),
+    mn_carbon_tot_pg_c_per_litre = sum(mn_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_litre = sum(mn_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_litre = sum(mn_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_pg_c_per_m3 = sum(mn_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_m3 = sum(mn_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_m3 = sum(mn_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_litre = sum(md_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_litre = sum(md_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_litre = sum(md_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_m3 = sum(md_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_m3 = sum(md_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    .groups = "drop") %>%# names()
+  dplyr::mutate(rbd = factor(rbd,
+                             levels = c(
+                               "Northumbria",
+                               "Humber",
+                               "Anglian",
+                               "Thames",
+                               "South East",
+                               "South West",
+                               "Severn",
+                               "North West",
+                               "Solway Tweed"
+                             )
+  )) %>% 
+  dplyr::mutate(wb_name = factor(wb_name,
+                                 levels = unique(wb_name[order(rbd)])
+  )
+  ) -> dfsummary_lf
+
+dfsummary_lf %>%
+  dplyr::filter(!is.na(phyto_lf)) %>% 
+  #dplyr::filter(rbd == "South East") %>% 
+  dplyr::filter(phyto_lf %in% c("Diatom","Dinoflagellate")) %>% 
+  dplyr::group_by(across(-c(cells_per_litre,
+                            cells_per_m3,
+                            mn_carbon_tot_pg_c_per_litre,
+                            mn_carbon_tot_ug_c_per_litre,
+                            mn_carbon_tot_mg_c_per_litre,
+                            mn_carbon_tot_pg_c_per_m3,
+                            mn_carbon_tot_ug_c_per_m3,
+                            mn_carbon_tot_mg_c_per_m3,
+                            md_carbon_tot_pg_c_per_litre,
+                            md_carbon_tot_ug_c_per_litre,
+                            md_carbon_tot_mg_c_per_litre,
+                            md_carbon_tot_pg_c_per_m3,
+                            md_carbon_tot_ug_c_per_m3,
+                            md_carbon_tot_mg_c_per_m3))) %>% 
+  dplyr::summarise(
+    cells_per_litre = mean(cells_per_litre, na.rm = TRUE),
+    cells_per_m3 = mean(cells_per_m3, na.rm = TRUE),
+    mn_carbon_tot_pg_c_per_litre = mean(mn_carbon_tot_pg_c_per_litre, na.rm = TRUE),
+    mn_carbon_tot_ug_c_per_litre = mean(mn_carbon_tot_ug_c_per_litre, na.rm = TRUE),
+    mn_carbon_tot_mg_c_per_litre = mean(mn_carbon_tot_mg_c_per_litre, na.rm = TRUE),
+    mn_carbon_tot_pg_c_per_m3 = mean(mn_carbon_tot_pg_c_per_m3, na.rm = TRUE),
+    mn_carbon_tot_ug_c_per_m3 = mean(mn_carbon_tot_ug_c_per_m3, na.rm = TRUE),
+    mn_carbon_tot_mg_c_per_m3 = mean(mn_carbon_tot_mg_c_per_m3, na.rm = TRUE),
+    md_carbon_tot_pg_c_per_litre = mean(md_carbon_tot_pg_c_per_litre, na.rm = TRUE),
+    md_carbon_tot_ug_c_per_litre = mean(md_carbon_tot_ug_c_per_litre, na.rm = TRUE),
+    md_carbon_tot_mg_c_per_litre = mean(md_carbon_tot_mg_c_per_litre, na.rm = TRUE),
+    md_carbon_tot_pg_c_per_m3 = mean(md_carbon_tot_pg_c_per_m3, na.rm = TRUE),
+    md_carbon_tot_ug_c_per_m3 = mean(md_carbon_tot_ug_c_per_m3, na.rm = TRUE),
+    md_carbon_tot_mg_c_per_m3 = mean(md_carbon_tot_mg_c_per_m3, na.rm = TRUE),
+    .groups = "drop"
+    ) %>% 
+  dplyr::mutate(
+    month = lubridate::month(sample_date),
+    year = lubridate::year(sample_date)) %>% 
+  dplyr::filter(year>2007) %>% 
+  dplyr::filter(rbd != "Severn") %>% 
+  dplyr::filter(rbd != "Solway Tweed") %>% 
+  ggplot(aes(
+    x = year,
+    y = month
+    # z = log10(md_carbon_tot_mg_c_per_m3+1)
+    )) +
+  # geom_point()
+  # stat_contour(geom="raster", interpolate=TRUE)+
+  # scale_fill_gradientn(colours=rev(rainbow(10)))+
+  # geom_tile(aes(fill=md_carbon_tot_mg_c_per_m3))+
+  # facet_wrap(~phyto_lf)
+  geom_hline(yintercept = c(1:12),lty = 2, colour="grey")+
+  geom_vline(xintercept = seq(from=2007.5, to=2025.5, by=1), lty = 2, colour="grey") +
+  geom_tile(aes(fill = log10(cells_per_m3)),show.legend = TRUE)+
+  # facet_wrap(.~phyto_lf,ncol = 1)+
+  facet_grid(rbd~phyto_lf)+
+  scale_fill_gradient(low = "lightskyblue2", high = "red")+
+  # scale_fill_gradientn(colours = c("blue", "white", "red"),
+  #                      values = scales::rescale(c(0,5,10)))+
+  labs(title = "Heatmaps of plankton abundances",
+       subtitle = "Data expressed as log10(cells per m3)",
+       x="",
+       y="Month")+
+  guides(fill=guide_legend(title=bquote(bold(Log[10]~Cells~m^-3)),
+                           reverse = TRUE))+
+  scale_y_continuous(breaks= scales::pretty_breaks())+
+  coord_cartesian(xlim = c(2008,NA),
+                  ylim = c(1,12))+
+theme(
+  axis.title = element_text(face=2),
+  axis.text = element_text(face=2),
+  strip.text = element_text(face=2),
+  legend.title = element_text(face=2)
+  )
+
+# by taxon ####
+
+plnk <- "Pseudo-nitzschia"
+mxCells <- log10(max(df0_raw$cells_per_m3[df0_raw$name_use == plnk],
+                     na.rm = TRUE))
+
+df0_raw %>%
+  dplyr::filter(grepl(plnk,name_use)) %>% 
+  dplyr::select(
+    rbd,
+    wbid, 
+    wb_name,
+    wb_type,
+    hm_desig,
+    area_name,
+    man_catch,
+    op_catch,
+    typology,
+    site_id, biosys_code, biosys_short, site_station_name,
+    eastings,northings,
+    sample_date,
+    replicate_code, 
+    colonies_per_litre_millilitre, analysis_type, analysis_method,
+    date_of_analysis, surveillan,
+    cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3) %>% 
+  dplyr::group_by(dplyr::across(-c(
+    cells_per_litre,
+    cells_per_m3,
+    mn_carbon_tot_pg_c_per_litre,
+    mn_carbon_tot_ug_c_per_litre,
+    mn_carbon_tot_mg_c_per_litre,
+    mn_carbon_tot_pg_c_per_m3,
+    mn_carbon_tot_ug_c_per_m3,
+    mn_carbon_tot_mg_c_per_m3,
+    md_carbon_tot_pg_c_per_litre,
+    md_carbon_tot_ug_c_per_litre,
+    md_carbon_tot_mg_c_per_litre,
+    md_carbon_tot_pg_c_per_m3,
+    md_carbon_tot_ug_c_per_m3,
+    md_carbon_tot_mg_c_per_m3
+  ))) %>% 
+  dplyr::summarise(
+    cells_per_litre = sum(cells_per_litre, na.rm = FALSE),
+    cells_per_m3 = sum(cells_per_m3, na.rm = FALSE),
+    mn_carbon_tot_pg_c_per_litre = sum(mn_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_litre = sum(mn_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_litre = sum(mn_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    mn_carbon_tot_pg_c_per_m3 = sum(mn_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_ug_c_per_m3 = sum(mn_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    mn_carbon_tot_mg_c_per_m3 = sum(mn_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_litre = sum(md_carbon_tot_pg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_litre = sum(md_carbon_tot_ug_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_litre = sum(md_carbon_tot_mg_c_per_litre, na.rm=TRUE),
+    md_carbon_tot_pg_c_per_m3 = sum(md_carbon_tot_pg_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3, na.rm=TRUE),
+    md_carbon_tot_mg_c_per_m3 = sum(md_carbon_tot_mg_c_per_m3, na.rm=TRUE),
+    .groups = "drop") %>%# names()
+  dplyr::mutate(rbd = factor(rbd,
+                             levels = c(
+                               "Northumbria",
+                               "Humber",
+                               "Anglian",
+                               "Thames",
+                               "South East",
+                               "South West",
+                               "Severn",
+                               "North West",
+                               "Solway Tweed"
+                             )
+  )) %>% 
+  dplyr::mutate(wb_name = factor(wb_name,
+                                 levels = unique(wb_name[order(rbd)])
+  )
+  ) %>% 
+  dplyr::mutate(
+    month = lubridate::month(sample_date),
+    year = lubridate::year(sample_date)) %>% 
+  dplyr::filter(year>2007) %>% 
+  dplyr::filter(rbd != "Severn") %>% 
+  dplyr::filter(rbd != "Solway Tweed") %>% 
+  ggplot(aes(
+    x = year,
+    y = month
+    )) +
+  geom_hline(yintercept = seq(0.5,12.5,by=1),lty = 2, colour="grey")+
+  geom_vline(xintercept = seq(from=2007.5, to=2025.5, by=1), lty = 2, colour="grey") +
+  geom_tile(aes(fill = log10(cells_per_m3)),show.legend = TRUE)+
+  facet_wrap(.~rbd)+
+  scale_fill_gradient(limits = c(0,mxCells),
+                      low = "lightskyblue2", high = "red")+
+  labs(title = paste0("Heatmaps of ",plnk," abundances"),
+       subtitle = bquote(Data~expressed~as~log[10]~cells~~m^-3),
+       x="",
+       y="Month")+
+  guides(fill=guide_legend(title=bquote(bold(Log[10]~Cells~m^-3)),
+                           reverse = TRUE))+
+  scale_y_continuous(breaks= scales::pretty_breaks())+
+  coord_cartesian(xlim = c(2008,NA),
+                  ylim = c(1,12))+
+  theme(
+    axis.title = element_text(face=2),
+    axis.text = element_text(face=2),
+    strip.text = element_text(face=2),
+    legend.title = element_text(face=2)
+  )
