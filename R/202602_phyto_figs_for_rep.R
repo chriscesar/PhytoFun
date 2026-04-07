f33a9b6a42923fd2300ec41be924977b558f593c
chriscesar <chris.cesar@environment-agency.gov.uk>
2026-04-07 14:35:02 +0100

    Generate summary table for report

--- R/202602_phyto_figs_for_rep.R
+++ R/202602_phyto_figs_for_rep.R
@@ -48,7 +48,10 @@ tictoc::toc()
 
 # summarise counts by sampling event & plot ####
 # png(file = paste0(outfol,"phyto_abund_log10.png"),
-png(file = paste0(outfol,"phyto_abund_raw.png"),
+png(file = paste0(outfol,
+                  # "phyto_abund_raw.png"
+                  "phyto_abund_log10.png"
+                  ),
     width=18*ppi, height=12*ppi, res=ppi)
 df %>%
   ## total abundance: cells per litre
@@ -69,8 +72,8 @@ df %>%
          aes(
            x = date_plot,
            # y = cells_per_litre
-           # y = log10(mn_cells_per_m3+1)
-           y=mn_cells_per_m3
+           y = log10(mn_cells_per_m3+1)
+           # y=mn_cells_per_m3
          )
          )+
   geom_rug(sides = "b")+
@@ -88,16 +91,17 @@ df %>%
                                     )
                                   ),
              lty = 3, col="grey")+
+  ylim(0,NA)+
   geom_point(aes(
     fill = n
     ),
     size=4,pch=21)+
   geom_smooth(method = "gam")+
   labs(
-    # title = "Log10 mean phytoplanton abundances by year_month",
-    title = "Mean phytoplanton abundances by year_month",
-    # y = "Log10(n+1) mean cell abundances (cells per m3) per sample",
-    y = "Mean cell abundances (cells per m3) per sample",
+    title = "Log10 mean phytoplanton abundances by year_month",
+    # title = "Mean phytoplanton abundances by year_month",
+    y = "Log10(n+1) mean cell abundances (cells per m3) per sample",
+    # y = "Mean cell abundances (cells per m3) per sample",
     caption=paste0("Values represent mean total cell counts across all samples gathered in a particular month","<br>",
                    "Point shading reflects number of samples contributing to that mean",
                    "<br>","Blue line represents generalised additive model trend"
@@ -126,11 +130,23 @@ dev.off()
 
 # summarise carbon by sampling event & plot ####
 # png(file = paste0(outfol,"phyto_carbon_raw.png"),
-png(file = paste0(outfol,"phyto_carbon_log10.png"),
+
+df$rbd <- factor(df$rbd, levels = c(
+  "Northumbria", "Humber","Anglian",
+  "Thames","South East","South West","Severn",
+  "North West", "Solway Tweed"
+  ))
+
+png(file = paste0(outfol,
+                  # "phyto_carbon_log10_by_rbd.png"
+                  "phyto_carbon_raw.png"
+                  # "phyto_carbon_log10.png"
+                  # "phyto_carbon_log10_by_rbd_with_line.png"
+                  ),
     width=18*ppi, height=12*ppi, res=ppi)
 df %>% #names()
   ## total carbon: cells per litre
-  select(sample_id,date_plot,md_carbon_tot_ug_c_per_m3) %>% 
+  select(sample_id,date_plot,md_carbon_tot_ug_c_per_m3,rbd) %>% 
   ##remove zero values
   dplyr::filter(!is.na(md_carbon_tot_ug_c_per_m3)) %>% 
   group_by(across(-md_carbon_tot_ug_c_per_m3)) %>% 
@@ -141,17 +157,19 @@ df %>% #names()
   dplyr::filter(md_carbon_tot_ug_c_per_m3 !=0) %>% 
   # get mean sum by date
   ## drop sample ID
-  select(-sample_id) %>% group_by(date_plot) %>%
+  # select(-sample_id) %>%
+  select(-sample_id,-rbd) %>%
+  group_by(across(-md_carbon_tot_ug_c_per_m3)) %>%
   # filter(!is.na(md_carbon_tot_mg_C_per_litre))
-  summarise(md_carbon_tot_ug_c_per_m3 = median(md_carbon_tot_ug_c_per_m3),
+  summarise(md_carbon_tot_ug_c_per_m3 = median(md_carbon_tot_ug_c_per_m3,na.rm=TRUE),
             n=n(),
-            .groups = "drop") %>% ungroup() %>% 
+            .groups = "drop") %>% ungroup() %>% #View()
   ggplot(.,
          aes(
            x = date_plot,
            # y = cells_per_litre
-           # y = md_carbon_tot_ug_c_per_m3,
-           y = log10(md_carbon_tot_ug_c_per_m3+1)
+           y = md_carbon_tot_ug_c_per_m3,
+           # y = log10(md_carbon_tot_ug_c_per_m3+1)
          )
   )+
   geom_rug(sides = "b")+
@@ -170,12 +188,15 @@ df %>% #names()
                                   ),
              lty = 3, col="grey")+
   geom_point(aes(fill = n),size=4,pch=21)+
+  # geom_hline(yintercept = 4,col=2,lwd=1.5)+
   geom_smooth(method = "gam")+
+  # facet_wrap(.~rbd)+
+  ylim(0,NA)+
   labs(
-    title = "Log10 median phytoplanton carbon content by year_month",
-    # title = "Median phytoplanton carbon content by year_month",
-    y = "Log10(n+1) median carbon content (um/m3) per sample",
-    # y = "Median carbon content (um/m3) per sample",
+    # title = "Log10 median phytoplanton carbon content by year_month",
+    title = "Median phytoplanton carbon content by year_month",
+    # y = "Log10(n+1) median carbon content (ug/m3) per sample",
+    y = "Median carbon content (ug/m3) per sample",
     caption=paste0("Values represent median total carbon contents across all samples gathered in a particular month","<br>",
                    "Point shading reflects number of samples contributing to that mean",
                    "<br>","Blue line represents generalised additive model trend"
@@ -187,19 +208,88 @@ df %>% #names()
     # colours = c("red", "yellow", "green", "yellow", "red"),
     # breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1)
     colours = c("#FEE0D2", "#FC9272", "#DE2D26"),
-    breaks = c(100, 200, 300)
+    # breaks = c(100, 200, 300)
   )+
   theme(
-    # palette.color.continuous = c("#FEE0D2", "#FC9272", "#DE2D26"),
+    palette.color.continuous = c("#FEE0D2", "#FC9272", "#DE2D26"),
     axis.title.x = element_blank(),
     axis.title.y = element_text(face=2,size=12),
     axis.text = element_text(face=2),
+    strip.text = element_text(face=2,size=12),
     axis.text.x = element_text(size = 12),
     plot.caption = ggtext::element_markdown(face=2,size=12),
     plot.title = element_text(face=2,size = 14),
     legend.title = ggtext::element_markdown(face=2),
   )
 dev.off()
+
+# create summary table ####
+# df is the by-taxon data for phytoplankton, containing variables for 
+# sample metadata, lifeform & taxon info, as well as the stuff we're more interested in:
+# cell abundance per unit volume,
+# estimated cell volume,
+# carbon content per cell (derived from mean & median carbon values when
+# a taxon fits > entries on Angus' & Tom's table)
+# these mean & median carbon values are multiplied by cell densities to give
+# carbon per unit volume.  These include carbon contents derived from both
+# mean & median carbon contents per cell.
+# Plots & summaries are based on the total carbon values in each sample, with
+# values a result of summing the individual carbon contents of each taxon per sample
+# The carbon values used are derived from the median carbon values per taxon
+# (when the table) contains >1 entry for a given taxon
+df %>% #names()
+  ## total carbon: cells per litre
+  select(sample_id, date_plot, md_carbon_tot_ug_c_per_m3, rbd) %>% 
+  ##remove zero values
+  dplyr::filter(!is.na(md_carbon_tot_ug_c_per_m3)) %>% 
+  group_by(across(-md_carbon_tot_ug_c_per_m3)) %>% 
+  # get sum per sample
+  summarise(md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3,
+                                            na.rm = TRUE),
+            .groups = "drop") %>% ungroup() %>% 
+  dplyr::filter(md_carbon_tot_ug_c_per_m3 !=0) %>% 
+  # get mean sum by date
+  ## drop sample ID
+  select(-sample_id) %>%
+  # select(-sample_id,-rbd) %>%
+  group_by(across(-md_carbon_tot_ug_c_per_m3)) %>%
+  # filter(!is.na(md_carbon_tot_mg_C_per_litre))
+  summarise(md_carbon_tot_ug_c_per_m3 = median(md_carbon_tot_ug_c_per_m3,na.rm=TRUE),
+            n=n(),
+            .groups = "drop") %>% ungroup() %>% 
+  group_by(rbd) %>% 
+  summarize(min = min(md_carbon_tot_ug_c_per_m3, na.rm = TRUE),
+            q1 = quantile(md_carbon_tot_ug_c_per_m3,0.25, na.rm = TRUE),
+            median = median(md_carbon_tot_ug_c_per_m3, na.rm = TRUE),
+            mean = mean(md_carbon_tot_ug_c_per_m3, na.rm = TRUE),
+            q3 = quantile(md_carbon_tot_ug_c_per_m3,0.75, na.rm = TRUE),
+            max=max(md_carbon_tot_ug_c_per_m3, na.rm = TRUE)) %>% 
+  write.csv(.,
+            file = paste0(outfol,"md_C_ug_per_m3_summary_by_rbd.csv"),
+            row.names = FALSE)
+
+df %>% #names()
+  ## total carbon: cells per litre
+  select(sample_id,date_plot,md_carbon_tot_ug_c_per_m3,rbd) %>% 
+  ##remove zero values
+  dplyr::filter(!is.na(md_carbon_tot_ug_c_per_m3)) %>% 
+  group_by(across(-md_carbon_tot_ug_c_per_m3)) %>% 
+  # get sum per sample
+  summarise(md_carbon_tot_ug_c_per_m3 = sum(md_carbon_tot_ug_c_per_m3,
+                                            na.rm = TRUE),
+            .groups = "drop") %>% ungroup() %>% 
+  dplyr::filter(md_carbon_tot_ug_c_per_m3 !=0) %>% 
+  # get mean sum by date
+  ## drop sample ID
+  # select(-sample_id) %>% 
+  write.csv(.,
+            file = paste0(outfol,"median_C_ug_per_m3_by_rbd.csv"),
+            row.names = FALSE)
+
+write.csv(df,
+          file = paste0(outfol,"md_C_ug_per_m3_raw.csv"),
+          row.names = FALSE)
+
 
 # chlorophyll content ####
 ## load data
